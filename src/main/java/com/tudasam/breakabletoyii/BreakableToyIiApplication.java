727534a5a0b4517f2b0a5583b39e504c2f5da069
roberto.tuda <roberto.tuda@encora.com>
2025-04-23 12:46:55 -0600

    initial

---
+++ src/main/java/com/tudasam/breakabletoyii/BreakableToyIiApplication.java
@@ -0,0 +1,13 @@
+package com.tudasam.breakabletoyii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreakableToyIiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreakableToyIiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tudasam/breakabletoyii/BreakableToyIiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tudasam.breakabletoyii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreakableToyIiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
