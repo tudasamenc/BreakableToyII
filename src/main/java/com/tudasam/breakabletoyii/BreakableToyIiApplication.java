c7a983eb18852a16111d052a3fe2d2dc91b2088e
roberto.tuda <roberto.tuda@encora.com>
2025-05-02 18:38:47 -0600

    Functional Backend 1

--- src/main/java/com/tudasam/breakabletoyii/BreakableToyIiApplication.java
+++ src/main/java/com/tudasam/breakabletoyii/BreakableToyIiApplication.java
@@ -2,6 +2,11 @@ package com.tudasam.breakabletoyii;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.client.web.client.OAuth2ClientHttpRequestInterceptor;
+import org.springframework.web.client.RestClient;
 
 @SpringBootApplication
 public class BreakableToyIiApplication {
@@ -10,4 +15,11 @@ public class BreakableToyIiApplication {
 		SpringApplication.run(BreakableToyIiApplication.class, args);
 	}
 
+	@Bean
+	public RestClient restClient(OAuth2AuthorizedClientManager authorizedClientManager) {
+		OAuth2ClientHttpRequestInterceptor interceptor = new OAuth2ClientHttpRequestInterceptor(authorizedClientManager);
+		return RestClient.builder()
+				.requestInterceptor(interceptor)
+				.build();
+	}
 }
