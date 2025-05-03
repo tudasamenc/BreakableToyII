e414b95b179a78598761246d01d619139ec5546d
roberto.tuda <roberto.tuda@encora.com>
2025-05-02 18:38:47 -0600

    Functional Backend 1

--- src/main/java/com/tudasam/breakabletoyii/config/SecurityConfig.java
+++ src/main/java/com/tudasam/breakabletoyii/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.client.RestTemplate;
 
@@ -22,9 +23,9 @@ public class SecurityConfig {
                         .requestMatchers("/**", "/login","/callback**","auth/spotify**").permitAll()
                         .anyRequest().authenticated()
                 )
-                .formLogin(Customizer.withDefaults())
-                .oauth2Login(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
                 .oauth2Client(Customizer.withDefaults());
+                //.oauth2Login(Customizer.withDefaults());
         return http.build();
     }
 
--- src/main/java/com/tudasam/breakabletoyii/controller/HomeController.java
+++ src/main/java/com/tudasam/breakabletoyii/controller/HomeController.java
@@ -1,30 +1,71 @@
 package com.tudasam.breakabletoyii.controller;
 
 
-import com.tudasam.breakabletoyii.SpotifyData.SpotifyArtist;
-
-
-import java.util.Arrays;
-import java.util.List;
-
-import com.tudasam.breakabletoyii.SpotifyData.SpotifyService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.*;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.RestClient;
+
+import static org.springframework.security.oauth2.client.web.client.RequestAttributeClientRegistrationIdResolver.clientRegistrationId;
 
 
 
 @RestController
-@CrossOrigin
 
 public class HomeController {
-    @Autowired
-    SpotifyService spotifyService;
-    private String codeCallback;
+
+    private final RestClient restClient;
+
+    public HomeController(RestClient restClient) {
+        this.restClient = restClient;
+    }
+    //GET Personal Info
+    @GetMapping("/me")
+    public String me() {
+        return restClient.get()
+                .uri("https://api.spotify.com/v1/me")
+                .attributes(clientRegistrationId("spotify"))
+                .retrieve()
+                .body(String.class);
+    }
+
+    //GET specific artist
+    @GetMapping("/artists/{id}")
+    String findArtist(@PathVariable String id){
+        return restClient.get()
+                .uri("https://api.spotify.com/v1/artists/"+id)
+                .attributes(clientRegistrationId("spotify"))
+                .retrieve()
+                .body(String.class);
+    }
+    //GET specific album
+    @GetMapping("/albums/{id}")
+    String findAlbum(@PathVariable String id){
+        return restClient.get()
+                .uri("https://api.spotify.com/v1/albums/"+id)
+                .attributes(clientRegistrationId("spotify"))
+                .retrieve()
+                .body(String.class);
+    }
+
+    //GET specific album tracks
+    @GetMapping("/albums/{id}/tracks")
+    String findAlbumTracks(@PathVariable String id){
+        return restClient.get()
+                .uri("https://api.spotify.com/v1/albums/"+id+"/tracks")
+                .attributes(clientRegistrationId("spotify"))
+                .retrieve()
+                .body(String.class);
+    }
+
+    // GET Most played artists
+    @GetMapping("/me/top/artists")
+    String getTopArtists(){
+        return restClient.get()
+                .uri("https://api.spotify.com/v1/me/top/artists")
+                .attributes(clientRegistrationId("spotify"))
+                .retrieve()
+                .body(String.class);
+    }
+
     // POST Authentication
     @PostMapping("/auth/spotify")
     String authenticateSpotify() {
@@ -34,36 +75,12 @@ public class HomeController {
         return "Authentication Successful";
     }
 
-    // GET Most played artists
-    @GetMapping("/me/top/artists")
-    List<SpotifyArtist> getTopArtists(OAuth2AuthenticationToken token) {
-        return getTopArtists(token);
-    }
-
-    //GET specific artist
-    @GetMapping("/artists/{id}")
-    String findArtist(@PathVariable String id){
-
-        return id;
-    }
-
-    @GetMapping("/ABC")
-    String testingAPI(){
-
-        return "ABC";
-
-    }
-
-
-
-
-
-
 
     @GetMapping("/")
     public String home() {
         return "Home";
     }
+
 
     @GetMapping("/secured")
     public String secured() {
@@ -72,12 +89,8 @@ public class HomeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code) {
-       // OAuth2User user = authentication.getPrincipal();
         System.out.println(code);
-        codeCallback = code;
-        //spotifyService.requestAccessToken(codeCallback);
         return "Welcome " + code;
     }
-
 
 }
