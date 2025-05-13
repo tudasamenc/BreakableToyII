222439f19e3f3d8cb60fc0978b0591dfdbcd39f4
roberto.tuda <roberto.tuda@encora.com>
2025-05-13 16:27:31 -0600

    Add URL

--- src/main/java/com/tudasam/breakabletoyii/BreakableToyIiApplication.java
+++ src/main/java/com/tudasam/breakabletoyii/BreakableToyIiApplication.java
@@ -31,6 +31,7 @@ public class BreakableToyIiApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/me").allowedOrigins("http://127.0.0.1:3000/").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+				registry.addMapping("/me/top/artists").allowedOrigins("http://127.0.0.1:3000/");
 				registry.addMapping("/").allowedOrigins("http://127.0.0.1:3000/");
 				registry.addMapping("/callback").allowedOrigins("http://127.0.0.1:3000/");
 				registry.addMapping("https://accounts.spotify.com/authorize").allowedOrigins("http://127.0.0.1:3000/");
